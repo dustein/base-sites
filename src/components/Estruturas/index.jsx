a7a8515494918bd0ce93adfdd1939b26161d5932
dustein <steindu@gmail.com>
2025-10-23 21:28:50 -0300

    font size footer

--- src/components/Estruturas/index.jsx
+++ src/components/Estruturas/index.jsx
@@ -3,73 +3,73 @@ export function Estruturas() {
 
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center gap-5">
 
-        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow w-64">
+        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow">
           <h3 className="font-bold text-xl mb-2 text-blue-800">Piscinas</h3>
           {/* <p className="text-gray-600">...</p> */}
           <img className="rounded-xl" src="imagens/piscina.jpg" alt="" />
         </div>
 
-        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow w-64">
+        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow">
           <h3 className="font-bold text-xl mb-2 text-blue-800">Saunas</h3>
           {/* <p className="text-gray-600">...</p> */}
           <img className="rounded-xl" src="imagens/sauna.jpg" alt="" />
         </div>
 
-        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow w-64">
+        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow">
           <h3 className="font-bold text-xl mb-2 text-blue-800">Parquinho</h3>
           {/* <p className="text-gray-600">...</p> */}
           <img className="rounded-xl" src="imagens/parquinho.jpg" alt="" />
         </div>
 
-        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow w-64">
+        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow">
           <h3 className="font-bold text-xl mb-2 text-blue-800">Quadra Poliesportiva</h3>
           {/* <p className="text-gray-600">...</p> */}
           <img className="rounded-xl" src="imagens/quadra-poliesportiva.jpg" alt="" />
         </div>
 
-        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow w-64">
+        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow">
           <h3 className="font-bold text-xl mb-2 text-blue-800">Quadra de Areia</h3>
           {/* <p className="text-gray-600">...</p> */}
           <img className="rounded-xl" src="imagens/quadra-areia.jpg" alt="" />
         </div>
 
-        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow w-64">
+        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow">
           <h3 className="font-bold text-xl mb-2 text-blue-800">Campo de Futebol</h3>
           {/* <p className="text-gray-600">...</p> */}
           <img className="rounded-xl" src="imagens/campo-futebol.jpg" alt="" />
         </div>
 
-        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow w-64">
+        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow">
           <h3 className="font-bold text-xl mb-2 text-blue-800">Quadras de Tênis</h3>
           {/* <p className="text-gray-600">...</p> */}
           <img className="rounded-xl" src="imagens/quadra-tenis.jpg" alt="" />
         </div>
 
-        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow w-64">
+        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow">
           <h3 className="font-bold text-xl mb-2 text-blue-800">Salão de Festas</h3>
           {/* <p className="text-gray-600">...</p> */}
           <img className="rounded-xl" src="imagens/salao-festas.jpg" alt="" />
         </div>
 
-        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow w-64">
+        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow">
           <h3 className="font-bold text-xl mb-2 text-blue-800">Churrasqueiras</h3>
           {/* <p className="text-gray-600">...</p> */}
           <img className="rounded-xl" src="imagens/churrasqueira.jpg" alt="" />
         </div>
 
-        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow w-64">
+        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow">
           <h3 className="font-bold text-xl mb-2 text-blue-800">Sinuca</h3>
           {/* <p className="text-gray-600">...</p> */}
           <img className="rounded-xl" src="imagens/sinuca.jpg" alt="" />
         </div>
 
-          <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow w-64">
+          <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow">
           <h3 className="font-bold text-xl mb-2 text-blue-800">Redário</h3>
           {/* <p className="text-gray-600">...</p> */}
           <img className="rounded-xl" src="imagens/redario.jpg" alt="" />
         </div>
 
-        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow w-64">
+        <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition-shadow">
           <h3 className="font-bold text-xl mb-2 text-blue-800">Estacionamento</h3>
           {/* <p className="text-gray-600">...</p> */}
           <img className="rounded-xl" src="imagens/estacionamento.jpg" alt="" />
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white">
-      <div className="text-sm container mx-auto px-4 py-8 text-center leading-6">
+      <div className="text-[0.6rem] md:text-sm container mx-auto px-4 py-8 text-center leading-6">
         <p>&copy; 2025 Country Club Novo Rio. Todos os direitos reservados.</p>
         <p> Av. Miguel Antônio Fernandes, 15 - Recreio dos Bandeirantes, Rio de Janeiro - RJ, 22790-682</p>
         <p>Telefone: 2490-2315 | 2490-1393 | 2490-7038</p>
